7e30ec6baaf84a91a3c025e0bb6e44cd0d4a5605
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-09-29 13:57:05 -0400

    Updated from Brython Server: 9/29/2018 1:57:04 PM

--- Q4.py
+++ Q4.py
@@ -1,7 +1,7 @@
 
 import math
 
-def costDesalinating(speed,size,dailyRental,numOfDays,disday1,disday2,disday3,disday4,disday5,disday6,disday7,disday8,disday9):
+def costDesalinating(speed,size,maxLoad,numOfDays,disday1,disday2,disday3,disday4,disday5,disday6,disday7,disday8,disday9):
 
     OneRate = [0.06,0.12,0.18,0.24]
     ThreeRate = [0.08,0.16,0.24,0.32]
@@ -27,9 +27,10 @@ def costDesalinating(speed,size,dailyRental,numOfDays,disday1,disday2,disday3,di
             rate += OneRate[(disday8/1000)-1]
         if disday9 > 0:
             rate += OneRate[(disday9/1000)-1]
-        print('rate =',rate)
+        rateVolume = (4/3)*3.14*rate**3
+        print('rate (volume/day) =',(4/3)*3.14*rate**3)
     
-    if speed == 3:
+    elif speed == 3:
         rate = 0
         if disday1 > 0:
             rate += ThreeRate[(disday1/1000)-1]
@@ -49,9 +50,10 @@ def costDesalinating(speed,size,dailyRental,numOfDays,disday1,disday2,disday3,di
             rate += ThreeRate[(disday8/1000)-1]
         if disday9 > 0:
             rate += ThreeRate[(disday9/1000)-1]
-        print('rate =',rate)
+        rateVolume = (4/3)*3.14*rate**3
+        print('rate (volume/day) =',(4/3)*3.14*rate**3)
         
-    if speed == 5:
+    elif speed == 5:
         rate = 0
         if disday1 > 0:
             rate += FiveRate[(disday1/1000)-1]
@@ -71,33 +73,43 @@ def costDesalinating(speed,size,dailyRental,numOfDays,disday1,disday2,disday3,di
             rate += FiveRate[(disday8/1000)-1]
         if disday9 > 0:
             rate += FiveRate[(disday9/1000)-1]
-        print('rate =',rate)
+        rateVolume = (4/3)*3.14*rate**3
+        print('rate (volume/day) =',rateVolume)
     
-    print('Cost of desalinating: $',(size-rate)*0.85*0.13)
+    print('Cost of desalinating: $',(size-rateVolume*numOfDays)*0.85*0.13)
     
     OneFuel = [10.9,13.7,16.4]
     ThreeFuel = [14,17.6,21]
     FiveFuel = [17.2,21.5,25.7]
     
-    if speed == 1:
-        if dailyRental == 520:
-            print('Cost of Towing = $',OneFuel[math.log10(size)-5]*5*9600+dailyRental*numOfDays)
-        else:
-            print('Cost of Towing = $',OneFuel[math.log10(size)-5]*9600+dailyRental*numOfDays)
+    dailyRental = [520,780,1400]
+    dailyRental1 = dailyRental[(math.log10(maxLoad)//1)-5]
     
-    if speed == 3:
-        if dailyRental == 520:
+    if maxLoad/size >= 1:
+        numOfLoads = maxLoad/size
+    else:
+        print('Not feasible')
+    
+    if speed == 1:
+        print('Cost of Towing = $',OneFuel[math.log10(size)-5]*numOfLoads*9600+dailyRental1*numOfDays)
+        '''else:
+            print('Cost of Towing = $',OneFuel[math.log10(size)-5]*9600+dailyRental*numOfDays)'''
+    
+    elif speed == 3:
+        print('Cost of Towing = $',OneFuel[math.log10(size)-5]*numOfLoads*9600+dailyRental1*numOfDays)
+        '''if dailyRental == 520:
             print('Cost of Towing = $',ThreeFuel[math.log10(size)-5]*5*9600+dailyRental*numOfDays)
         else:
-            print('Cost of Towing = $',ThreeFuel[math.log10(size)-5]*9600+dailyRental*numOfDays)
+            print('Cost of Towing = $',ThreeFuel[math.log10(size)-5]*9600+dailyRental*numOfDays)'''
     
-    if speed == 5:
-        if dailyRental == 520:
+    elif speed == 5:
+        print('Cost of Towing = $',OneFuel[math.log10(size)-5]*numOfLoads*9600+dailyRental1*numOfDays)
+        '''if dailyRental == 520:
             print('Cost of Towing = $',FiveFuel[math.log10(size)-5]*5*9600+dailyRental*numOfDays)
         else:
-            print('Cost of Towing = $',FiveFuel[math.log10(size)-5]*9600+dailyRental*numOfDays)
+            print('Cost of Towing = $',FiveFuel[math.log10(size)-5]*9600+dailyRental*numOfDays)'''
         
-costDesalinating(1,100000,520,2,3000,3000,2000,2000,0,0,0,0,0)
+costDesalinating(1,100000,500000,4,3000,3000,2000,2000,0,0,0,0,0)
 
 '''def costTowing(speed,size,dailyRental,numOfDays):
 
