66130d9ed561efc7ce20b7f71a5bddf26090ad95
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-10-01 10:50:06 -0400

    Updated from Brython Server: 10/1/2018 10:50:04 AM

--- Q4.py
+++ Q4.py
@@ -119,6 +119,7 @@ for x2 in range(0,168):
     #prints function and interval
     radius = eval(r.replace('x',str(x2)))
     volume -= (4/3)*3.1415*(radius)**3
+    print(volume)
     f = '2.75*math.log10(volume)-2.83'
     fuel = eval(f.replace('x',str(volume)))
     costOfFuel_Before4000 += fuel*24
