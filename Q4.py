de4ad743d55adbd5f06e4e94ee1d10e6319e1205
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-09-30 21:00:53 -0400

    Updated from Brython Server: 9/30/2018 9:00:52 PM

--- Q4.py
+++ Q4.py
@@ -1,7 +1,7 @@
 
 import math
 
-def cost(speed,size,maxLoad):
+'''def cost(speed,size,maxLoad):
 
     radius1 = 76
     radius3 = 60
@@ -123,7 +123,37 @@ def cost(speed,size,maxLoad):
         print('Cost of Towing = $',costOfTowing)
         print('Profit =',(size-totalVolumeLoss)*0.85*0.13 - costOfTowing)
 
-cost(5,10000000,10000000)
+cost(5,10000000,10000000)'''
+
+x2 = 0
+    f = '0.00144*x-0.0000014'
+    eval(f.replace('x',str(x2)))
+    #prints function and interval
+    print('Interval = [',0,',',x2,']')
+    
+    #Finding derivative
+    print("f'(x) = ",a*b,'x^',b-1,' + ',c*d,'x^',d-1)
+    
+    #number of boxes, after 1000 boxes the program runs very slowly
+    n = 100
+    
+    #a loop multiplying every other y value by 4 and the other y values by 2 (Simpson's Rule)
+    num = 1
+    total = 0
+    for i in range(1,n):
+        if num%2 == 0:
+            total += 4*eval(f.replace('x',str(((x2-0)/n)*i)))
+            num += 1
+        else:
+            total += 2*eval(f.replace('x',str(((x2-0)/n)*i)))
+            num += 1
+    
+    #first value yo and last value yn
+    firstYValue = sqrt(1+(a*b*(x1)**(b-1)+c*d*(x1)**(d-1))**2)
+    lastYValue = sqrt(1+(a*b*(x2)**(b-1)+c*d*(x2)**(d-1))**2)
+    
+    #prints arc length
+    print('Arc Length:',(total+firstYValue+lastYValue)*(x2-x1)/n*(1/3))
 
     
         
