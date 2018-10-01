9c2881117f83fe5fbeed9c39aeb0994602e95e18
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-09-30 21:15:38 -0400

    Updated from Brython Server: 9/30/2018 9:15:37 PM

--- Q4.py
+++ Q4.py
@@ -126,8 +126,8 @@ import math
 cost(5,10000000,10000000)'''
 
 
-for x2 in range(0,5):
-    f = '0.00144*x-0.0000014'
+for x2 in range(0,20):
+    f = '(4/3)*3.1415*(0.00144*x-0.0000014)**3'
     eval(f.replace('x',str(x2)))
     #prints function and interval
     print('Interval = [',0,',',x2,']')
@@ -153,7 +153,7 @@ for x2 in range(0,5):
     integral = (total+firstYValue+lastYValue)*(x2)/n*(1/3)
     
     #prints arc length
-    print('Integral:',integral)
+print('Integral:',integral)
 
     
         
