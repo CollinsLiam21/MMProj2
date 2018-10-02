564f8dbeb70f8f11224c3b92276bcd4f5d179d45
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-10-01 20:30:00 -0400

    Updated from Brython Server: 10/1/2018 8:29:59 PM

--- Q4.py
+++ Q4.py
@@ -105,7 +105,7 @@ cost(1,10000000,10000000)'''
 cost(1,10000000,10000000)'''
 
 maxLoad = 10000000
-speed = 5
+speed = 3
 
 dailyRental = [520,780,1400]
 dailyRental1 = dailyRental[(math.log10(maxLoad)//1)-5]
@@ -114,18 +114,18 @@ numOfDays = round((9600/speed)/24)
 print(numOfDays)
 
 if speed == 1:
-    radius1 = 133.65046
-    volume = maxLoad
+    radius1 = 133.65046     #starting radius of spherical iceberg (found using volume of starting size)
+    volume = maxLoad    #starting volume is the assumption that the starting size of the iceberg is the max load
     costOfFuel_Before4000 = 0
-    for x2 in range(1,168):
-        r = '0.00144*x-0.0000014'
-        #prints function and interval
-        radius = eval(r.replace('x',str(x2)))
-        radius1 -= radius
-        volume = (4/3)*3.1415*(radius1)**3
-        f = '2.75*math.log10(x)-2.83'
-        fuel = eval(f.replace('x',str(volume)))
-        costOfFuel_Before4000 += fuel*24
+    for x2 in range(1,168):     #for loop that calculates volume at the end of 4000km and total cost of fuel up to 4000km
+        r = '0.00144*x-0.0000014'   #linear regression of rate iceberg shrinks up to 4000km - found in desmos
+        radius = eval(r.replace('x',str(x2)))   #plugs day into linear regression to find rate of decrease at that specific days
+        radius1 -= radius   #updates radius value by subtracting radius on day x2 from previous day
+        volume = (4/3)*3.1415*(radius1)**3      #calculates volume of iceberg at day x2
+        f = '2.75*math.log10(x)-2.83'    #linear regression of fuel cost (made linear by taking log of size) - found in demos
+        fuel = eval(f.replace('x',str(volume)))      #finds cost of fuel at day x2 based on the volume at that day
+        costOfFuel_Before4000 += fuel*24    #updates total cost of fuel by adding the fuel for each day and..
+                                                #multiplying by 24 because boat travels 24km/day at 1km/hr
     
     
     costOfFuel_After4000 = 0
@@ -161,7 +161,7 @@ elif speed == 3:
         radius1 -= radius   #updates radius value by subtracting radius on day x2 from previous day
         volume = (4/3)*3.1415*(radius1)**3      #calculates volume of iceberg at day x2
         f = '3.5*math.log10(x)-3.467'   #linear regression of fuel cost (made linear by taking log of size) - found in demos
-        fuel = eval(f.replace('x',str(volume)))     #finds cost of fuel at day x2
+        fuel = eval(f.replace('x',str(volume)))     #finds cost of fuel at day x2 based on the volume at that day
         costOfFuel_Before4000 += fuel*72    #updates total cost of fuel by adding the fuel for each day and..
                                                 #multiplying by 72 because boat travels 72km/day at 3km/hr
     
@@ -172,7 +172,7 @@ elif speed == 3:
         radius1 -= radius   #updates radius value by subtracting radius on day x2 from previous day
         volume = (4/3)*3.1415*(radius1)**3      #calculates volume of iceberg at day x2
         f = '3.5*math.log10(x)-3.467'   #same regression for fuel cost b/c fuel cost continues to change
-        fuel = eval(f.replace('x',str(volume)))     #finds cost of fuel at day x2
+        fuel = eval(f.replace('x',str(volume)))     #finds cost of fuel at day x2 based on the volume at that day
         costOfFuel_After4000 += fuel*72     #updates total cost of fuel by adding the fuel for each day and..
                                                 #multiplying by 72 because boat travels 72km/day at 3km/hr
     
@@ -199,7 +199,7 @@ elif speed == 5:
         radius1 -= radius   #updates radius value by subtracting radius on day x2 from previous day
         volume = (4/3)*3.1415*(radius1)**3      #calculates volume of iceberg at day x2
         f = '4.25*math.log10(x)-4.033'   #linear regression of fuel cost (made linear by taking log of size) - found in demos
-        fuel = eval(f.replace('x',str(volume)))     #finds cost of fuel at day x2
+        fuel = eval(f.replace('x',str(volume)))     #finds cost of fuel at day x2 based on the volume at that day
         costOfFuel_Before4000 += fuel*120    #updates total cost of fuel by adding the fuel for each day and..
                                                 #multiplying by 120 because boat travels 120km/day at 5km/hr
     
@@ -210,7 +210,7 @@ elif speed == 5:
         radius1 -= radius   #updates radius value by subtracting radius on day x2 from previous day
         volume = (4/3)*3.1415*(radius1)**3      #calculates volume of iceberg at day x2
         f = '4.25*math.log10(x)-4.033'   #same regression for fuel cost b/c fuel cost continues to change
-        fuel = eval(f.replace('x',str(volume)))     #finds cost of fuel at day x2
+        fuel = eval(f.replace('x',str(volume)))     #finds cost of fuel at day x2 based on the volume at that day
         costOfFuel_After4000 += fuel*120    #updates total cost of fuel by adding the fuel for each day and..
                                                 #multiplying by 120 because boat travels 120km/day at 5km/hr
     
